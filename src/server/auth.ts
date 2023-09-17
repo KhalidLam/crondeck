7dcd2a6244bd7606958facbe380695e14f7f08d0
Lamsadi khalid <khalid@lafactory.co>
2023-09-18 00:00:24 +0100

    fix build issues && change db provider to postgresql

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,6 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "@/env.mjs";
 import { db } from "@/server/db";
@@ -48,10 +47,6 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     /**
      * ...add more providers here.
      *
